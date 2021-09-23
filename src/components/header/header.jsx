b6d4f9b0549eed1b0817b96e7dd3f262ca1a2e82
dlwhd990 <dlwhd990@gmail.com>
2021-09-23 14:08:55 +0900

    header drop down menu

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,18 +1,153 @@
-import React from "react";
+import React, { useState } from "react";
+import { useHistory } from "react-router";
+import { useEffect } from "react/cjs/react.development";
 import styles from "./header.module.css";
 
 const Header = (props) => {
+  const [aboutDropDown, setAboutDropDown] = useState(false);
+  const [programDropDown, setProgramDropDown] = useState(false);
+  const [fnqDropDown, setFnqDropDown] = useState(false);
+  const [contactDropDown, setContactDropDown] = useState(false);
+  const [navbarMouseEnter, setNavbarMouseEnter] = useState(false);
+  const [dropDownMouseEnter, setDropDownMouseEnter] = useState(false);
+
+  const history = useHistory();
+
+  const aboutDropDownOn = () => {
+    setAboutDropDown(true);
+    setProgramDropDown(false);
+    setFnqDropDown(false);
+    setContactDropDown(false);
+  };
+
+  const programDropDownOn = () => {
+    setAboutDropDown(false);
+    setProgramDropDown(true);
+    setFnqDropDown(false);
+    setContactDropDown(false);
+  };
+
+  const fnqDropDownOn = () => {
+    setAboutDropDown(false);
+    setProgramDropDown(false);
+    setFnqDropDown(true);
+    setContactDropDown(false);
+  };
+
+  const contactDropDownOn = () => {
+    setAboutDropDown(false);
+    setProgramDropDown(false);
+    setFnqDropDown(false);
+    setContactDropDown(true);
+  };
+
+  useEffect(() => {
+    if (!dropDownMouseEnter && !navbarMouseEnter) {
+      setAboutDropDown(false);
+      setProgramDropDown(false);
+      setFnqDropDown(false);
+      setContactDropDown(false);
+    }
+  }, [dropDownMouseEnter, navbarMouseEnter]);
+
   return (
     <section className={styles.header}>
       <div className={styles.logo_container}>
-        <span className={styles.logo}>모두락</span>
+        <span
+          className={styles.logo}
+          onClick={() => {
+            history.push("/");
+            window.scrollTo({ top: 0 });
+          }}
+        >
+          모두락
+        </span>
       </div>
-      <nav className={styles.navbar}>
+      <nav
+        className={styles.navbar}
+        onMouseEnter={() => {
+          setNavbarMouseEnter(true);
+        }}
+        onMouseLeave={() => {
+          setNavbarMouseEnter(false);
+        }}
+      >
         <ul className={styles.menu_list}>
-          <li className={styles.menu_item}>About</li>
-          <li className={styles.menu_item}>Program</li>
-          <li className={styles.menu_item}>F&Q</li>
-          <li className={styles.menu_item}>Contact</li>
+          <li className={styles.menu_item} onMouseEnter={aboutDropDownOn}>
+            <div className={styles.menu_item_title}>About</div>
+            <ul
+              className={`${
+                aboutDropDown
+                  ? `${styles.drop_down_menu} ${styles.on}`
+                  : `${styles.drop_down_menu} ${styles.off}`
+              }`}
+              onMouseEnter={() => {
+                setDropDownMouseEnter(true);
+              }}
+              onMouseLeave={() => {
+                setDropDownMouseEnter(false);
+              }}
+            >
+              <li className={styles.drop_down_menu_item}>회사소개</li>
+            </ul>
+          </li>
+          <li className={styles.menu_item} onMouseEnter={programDropDownOn}>
+            <div className={styles.menu_item_title}>Program</div>
+            <ul
+              className={`${
+                programDropDown
+                  ? `${styles.drop_down_menu} ${styles.on}`
+                  : `${styles.drop_down_menu} ${styles.off}`
+              }`}
+              onMouseEnter={() => {
+                setDropDownMouseEnter(true);
+              }}
+              onMouseLeave={() => {
+                setDropDownMouseEnter(false);
+              }}
+            >
+              <li className={styles.drop_down_menu_item}>프로그램소개</li>
+              <li className={styles.drop_down_menu_item}>미정</li>
+              <li className={styles.drop_down_menu_item}>미정</li>
+            </ul>
+          </li>
+          <li className={styles.menu_item} onMouseEnter={fnqDropDownOn}>
+            <div className={styles.menu_item_title}>F&Q</div>
+            <ul
+              className={`${
+                fnqDropDown
+                  ? `${styles.drop_down_menu} ${styles.on}`
+                  : `${styles.drop_down_menu} ${styles.off}`
+              }`}
+              onMouseEnter={() => {
+                setDropDownMouseEnter(true);
+              }}
+              onMouseLeave={() => {
+                setDropDownMouseEnter(false);
+              }}
+            >
+              <li className={styles.drop_down_menu_item}>공지사항</li>
+              <li className={styles.drop_down_menu_item}>문의게시판</li>
+            </ul>
+          </li>
+          <li className={styles.menu_item} onMouseEnter={contactDropDownOn}>
+            <div className={styles.menu_item_title}>Contact</div>
+            <ul
+              className={`${
+                contactDropDown
+                  ? `${styles.drop_down_menu} ${styles.on}`
+                  : `${styles.drop_down_menu} ${styles.off}`
+              }`}
+              onMouseEnter={() => {
+                setDropDownMouseEnter(true);
+              }}
+              onMouseLeave={() => {
+                setDropDownMouseEnter(false);
+              }}
+            >
+              <li className={styles.drop_down_menu_item}>연락처</li>
+            </ul>
+          </li>
         </ul>
       </nav>
       <div className={styles.login_container}>
